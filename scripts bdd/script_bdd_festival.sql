c2d15348dfa813ca292383e8e00da35c3a5fff93
zaky-nano <55179879+zaky-nano@users.noreply.github.com>
2019-09-30 00:18:09 +0200

    Add files via upload
    ajout des scripts de la bdd

---
+++ scripts bdd/script_bdd_festival.sql
@@ -0,0 +1,167 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  Dim 29 sep. 2019 à 22:14
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `festival`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `attribution`
+--
+
+DROP TABLE IF EXISTS `attribution`;
+CREATE TABLE IF NOT EXISTS `attribution` (
+  `idEtab` char(8) NOT NULL,
+  `idGroupe` char(4) NOT NULL,
+  `nombreChambres` int(11) NOT NULL,
+  PRIMARY KEY (`idEtab`,`idGroupe`),
+  KEY `fk2_Attribution` (`idGroupe`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `attribution`
+--
+
+INSERT INTO `attribution` (`idEtab`, `idGroupe`, `nombreChambres`) VALUES
+('0350123A', 'g004', 13),
+('0350123A', 'g005', 8),
+('0350785N', 'g001', 11),
+('0350785N', 'g002', 9),
+('0351234W', 'g001', 3),
+('0351234W', 'g006', 10),
+('0351234W', 'g007', 7);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etablissement`
+--
+
+DROP TABLE IF EXISTS `etablissement`;
+CREATE TABLE IF NOT EXISTS `etablissement` (
+  `id` char(8) NOT NULL,
+  `nom` varchar(45) NOT NULL,
+  `adresseRue` varchar(45) NOT NULL,
+  `codePostal` char(5) NOT NULL,
+  `ville` varchar(35) NOT NULL,
+  `tel` varchar(13) NOT NULL,
+  `adresseElectronique` varchar(70) DEFAULT NULL,
+  `type` tinyint(4) NOT NULL,
+  `civiliteResponsable` varchar(12) NOT NULL,
+  `nomResponsable` varchar(25) NOT NULL,
+  `prenomResponsable` varchar(25) DEFAULT NULL,
+  `nombreChambresOffertes` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `etablissement`
+--
+
+INSERT INTO `etablissement` (`id`, `nom`, `adresseRue`, `codePostal`, `ville`, `tel`, `adresseElectronique`, `type`, `civiliteResponsable`, `nomResponsable`, `prenomResponsable`, `nombreChambresOffertes`) VALUES
+('0350123A', 'Collège Lamartine', '3, avenue des corsaires', '35404', 'Paramé', '0299561459', '', 1, 'Mme', 'Lefort', 'Anne', 58),
+('0350785N', 'Collège de Moka', '2 avenue Aristide Briand BP 6', '35401', 'Saint-Malo', '0299206990', NULL, 1, 'M.', 'Dupont', 'Alain', 20),
+('0351234W', 'Collège Léonard de Vinci', '2 rue Rabelais', '35418', 'Saint-Malo', '0299117474', NULL, 1, 'M.', 'Durand', 'Pierre', 60);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groupe`
+--
+
+DROP TABLE IF EXISTS `groupe`;
+CREATE TABLE IF NOT EXISTS `groupe` (
+  `id` char(4) NOT NULL,
+  `nom` varchar(40) NOT NULL,
+  `identiteResponsable` varchar(40) DEFAULT NULL,
+  `adressePostale` varchar(120) DEFAULT NULL,
+  `nombrePersonnes` int(11) NOT NULL,
+  `nomPays` varchar(40) NOT NULL,
+  `hebergement` char(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `groupe`
+--
+
+INSERT INTO `groupe` (`id`, `nom`, `identiteResponsable`, `adressePostale`, `nombrePersonnes`, `nomPays`, `hebergement`) VALUES
+('g001', 'Groupe folklorique du Bachkortostan', NULL, NULL, 40, 'Bachkirie', 'O'),
+('g002', 'Marina Prudencio Chavez', NULL, NULL, 25, 'Bolivie', 'O'),
+('g003', 'Nangola Bahia de Salvador', NULL, NULL, 34, 'Brésil', 'O'),
+('g004', 'Bizone de Kawarma', NULL, NULL, 38, 'Bulgarie', 'O'),
+('g005', 'Groupe folklorique camerounais', NULL, NULL, 22, 'Cameroun', 'O'),
+('g006', 'Syoung Yaru Mask Dance Group', NULL, NULL, 29, 'Corée du Sud', 'O'),
+('g007', 'Pipe Band', NULL, NULL, 19, 'Ecosse', 'O'),
+('g008', 'Aira da Pedra', NULL, NULL, 5, 'Espagne', 'O'),
+('g009', 'The Jersey Caledonian Pipe Band', NULL, NULL, 21, 'Jersey', 'O'),
+('g010', 'Groupe folklorique des Émirats', NULL, NULL, 30, 'Emirats arabes unis', 'O'),
+('g011', 'Groupe folklorique mexicain', NULL, NULL, 38, 'Mexique', 'O'),
+('g012', 'Groupe folklorique de Panama', NULL, NULL, 22, 'Panama', 'O'),
+('g013', 'Groupe folklorique papou', NULL, NULL, 13, 'Papouasie', 'O'),
+('g014', 'Paraguay Ete', NULL, NULL, 26, 'Paraguay', 'O'),
+('g015', 'La Tuque Bleue', NULL, NULL, 8, 'Québec', 'O'),
+('g016', 'Ensemble Leissen de Oufa', NULL, NULL, 40, 'République de Bachkirie', 'O'),
+('g017', 'Groupe folklorique turc', NULL, NULL, 40, 'Turquie', 'O'),
+('g018', 'Groupe folklorique russe', NULL, NULL, 43, 'Russie', 'O'),
+('g019', 'Ruhunu Ballet du village de Kosgoda', NULL, NULL, 27, 'Sri Lanka', 'O'),
+('g020', 'L\'Alen', NULL, NULL, 34, 'France - Provence', 'O'),
+('g021', 'L\'escolo Di Tourre', NULL, NULL, 40, 'France - Provence', 'O'),
+('g022', 'Deloubes Kévin', NULL, NULL, 1, 'France - Bretagne', 'O'),
+('g023', 'Daonie See', NULL, NULL, 5, 'France - Bretagne', 'O'),
+('g024', 'Boxty', NULL, NULL, 5, 'France - Bretagne', 'O'),
+('g025', 'Soeurs Chauvel', NULL, NULL, 2, 'France - Bretagne', 'O'),
+('g026', 'Cercle Gwik Alet', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g027', 'Bagad Quic En Groigne', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g028', 'Penn Treuz', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g029', 'Savidan Launay', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g030', 'Cercle Boked Er Lann', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g031', 'Bagad Montfortais', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g032', 'Vent de Noroise', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g033', 'Cercle Strollad', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g034', 'Bagad An Hanternoz', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g035', 'Cercle Ar Vro Melenig', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g036', 'Cercle An Abadenn Nevez', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g037', 'Kerc\'h Keltiek Roazhon', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g038', 'Bagad Plougastel', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g039', 'Bagad Nozeganed Bro Porh-Loeiz', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g040', 'Bagad Nozeganed Bro Porh-Loeiz', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g041', 'Jackie Molard Quartet', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g042', 'Deomp', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g043', 'Cercle Olivier de Clisson', NULL, NULL, 0, 'France - Bretagne', 'N'),
+('g044', 'Kan Tri', NULL, NULL, 0, 'France - Bretagne', 'N');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `attribution`
+--
+ALTER TABLE `attribution`
+  ADD CONSTRAINT `fk1_Attribution` FOREIGN KEY (`idEtab`) REFERENCES `etablissement` (`id`),
+  ADD CONSTRAINT `fk2_Attribution` FOREIGN KEY (`idGroupe`) REFERENCES `groupe` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
